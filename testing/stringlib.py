e6e4a96c5220eeaa1ffcef235cdb7a7549c8b458
robuntu <robuntu@stro.ycdsb.ca>
2018-10-30 11:06:05 -0400

    finish TTD testing

--- testing/stringlib.py
+++ testing/stringlib.py
@@ -1,4 +1,7 @@
-def lettercount(myStr):
-    pass
+def letterCount(myStr, letter):
+    if len(myStr) <= 0 or len(letter) <= 0:
+        return 0
+    return myStr.count(letter)
 
-
+def removeSpace(myStr):
+    return myStr.replace(" ", "")
--- testing/test_stringlib.py
+++ testing/test_stringlib.py
@@ -1,13 +1,29 @@
 import pytest
-from stringlib import *
+from testing.stringlib import *
 
 def test_lettercount_basic1():
-    assert (lettercount("hi") == 2)
+    assert(letterCount("hi", "i") == 1)
 
 def test_lettercount_basic2():
-    assert (lettercount("fight me") == 8)
+    assert(letterCount("heyyo buddy", "y") == 3)
+
+def test_lettercount_emptyletter():
+    assert(letterCount("pop goes the weasel", "") == 0)
+
+def test_lettercount_emptystr():
+    assert(letterCount("", "o") == 0)
 
 def test_lettercount_empty():
-    assert (lettercount("") == 0)
+    assert(letterCount("", "") == 0)
 
+def test_removeSpace_basic1():
+    assert(removeSpace("Hey everybody!") == "Heyeverybody!")
 
+def test_removeSpace_basic2():
+    assert(removeSpace("Hi!    hi") == "Hi!hi")
+
+def test_removeSpace_emptySpace():
+    assert(removeSpace("  ") == "")
+
+def test_removeSpace_emptyStr():
+    assert(removeSpace("") == "")
